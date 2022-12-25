67419f437be9d78ad005dc76e3c5ac151ab89644
sayyed-ahsan <syed.jaid.7748@gmail.com>
2022-12-25 19:25:20 +0600

    tawilnd is fixed

--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,7 @@ import { Button } from "@material-ui/core";
 export default function Home() {
   return (
     <>
-      <Button variant="contained" color="primary">
-        Hello World
-      </Button>
+
     </>
   )
 }
