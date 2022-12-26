e37464df818784f3512cbd55962ae3fc9c02f2f4
sayyed-ahsan <syed.jaid.7748@gmail.com>
2022-12-26 19:27:24 +0600

    header is add

--- Components/singleParts/nav.js
+++ Components/singleParts/nav.js
@@ -1,20 +1,16 @@
-import React from 'react';
 
 const nav = () => {
     return (
         <div className="navbar bg-base-100">
             <div className="flex-1">
-                <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+                <a className="btn btn-ghost normal-case text-xl"><img src='/Logo.png' alt="" /></a>
             </div>
             <div className="flex-none">
-                <ul className="menu menu-horizontal px-1">
+                <ul className="menu menu-horizontal px-1 font-semibold">
                     <li><a>Product</a></li>
-                    <li><a></a></li>
                     <li><a>Services</a></li>
-                    <li><a></a></li>
                     <li><a>About</a></li>
-                    <li><a></a></li>
-                    <button className="btn btn-outline lowercase">Log In</button>
+                    <li className="px-[40px] pt-[8px] h-[49px] border-2 rounded-full border-gray-900 ml-[10px]">log In</li>
                 </ul>
             </div>
         </div>
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Landingpage from '../Components/landingpage'
 
 export default function Home() {
   return (
-    <div className='py-[30px] px-[50px]'>
+    <div className='py-[50px] px-[100px]'>
       <Landingpage></Landingpage>
     </div>
   )
