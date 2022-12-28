26b4a598cf1b452964b399c08326d2726c30302d
sayyed-ahsan <syed.jaid.7748@gmail.com>
2022-12-28 20:51:24 +0600

    about img is add

--- pages/about.js
+++ pages/about.js
@@ -2,6 +2,12 @@ import React from 'react'
 
 export default function about() {
     return (
-        <div>about</div>
+        <div>about
+            <img src="/sl1" alt="" />
+            <img src="/sl2" alt="" />
+            <img src="/sm" alt="" />
+            <img src="/sr1" alt="" />
+            <img src="/sr2" alt="" />
+        </div>
     )
 }
