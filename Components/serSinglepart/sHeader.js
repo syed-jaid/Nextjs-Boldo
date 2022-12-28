3f07e19d635036f6a8923a67201a3f79e824a4ad
sayyed-ahsan <syed.jaid.7748@gmail.com>
2022-12-28 20:27:17 +0600

    service add img

--- Components/serSinglepart/sHeader.js
+++ Components/serSinglepart/sHeader.js
@@ -6,6 +6,11 @@ export default function sHeader() {
             <p className='text-center text-[#FFFFFF] text-[20px]'>About</p>
             <p className='text-center text-[#FFFFFF] text-[64px]'>We love to make great <br /> things, things that matter.</p>
             <p className='text-center text-[#FFFFFF] text-[16px] mt-[30px]'>Funding handshake buyer business-to-business metrics iPad partnership. First mover advantage <br /> innovator success deployment non-disclosure.</p>
+            <img src="/sl1" alt="" />
+            <img src="/sl2" alt="" />
+            <img src="/sm" alt="" />
+            <img src="/sr1" alt="" />
+            <img src="/sr2" alt="" />
         </div>
     )
 }
